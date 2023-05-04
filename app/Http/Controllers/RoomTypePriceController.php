aa424b1be2872b53740727434035eb03176279bd
adsy2010 <adsy2010@users.noreply.github.com>
2023-05-04 21:30:25 +0100

    Initial database structure

---
+++ app/Http/Controllers/RoomTypePriceController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RoomTypePrice;
+use App\Http\Requests\StoreRoomTypePriceRequest;
+use App\Http\Requests\UpdateRoomTypePriceRequest;
+
+class RoomTypePriceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRoomTypePriceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RoomTypePrice $roomTypePrice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RoomTypePrice $roomTypePrice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRoomTypePriceRequest $request, RoomTypePrice $roomTypePrice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RoomTypePrice $roomTypePrice)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_05_03_204917_create_people_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\User::class);
+            $table->foreignIdFor(\App\Models\Role::class);
+            $table->foreignIdFor(\App\Models\Hotel::class);
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->date('date_of_birth');
+            $table->date('gender');
+            $table->string('contact_number');
+            $table->json('address');
+            $table->string('postcode');
+            $table->string('country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('people');
+    }
+};
